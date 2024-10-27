cd304ce53f80cf863ae52df61ef96f30e97e97f2
Acacia-Sanchez-Pastur <sanchezpastur@gmail.com>
2024-10-27 10:55:20 +0100

    initialized repository, created and filled out README.md

---
+++ src/main/java/org/factoriaf5/javaimc/JavaimcApplication.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.javaimc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaimcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaimcApplication.class, args);
+	}
+
+}
